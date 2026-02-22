1a0e96c8aca1a77912ac026adc96c8cda57cc70d
Soumya Sinha <soumyasinha161093@gmail.com>
2026-02-22 07:03:02 +0530

    Making fetch_pubmed_articles dynamic

--- src/pubmed_loader.py
+++ src/pubmed_loader.py
@@ -1,23 +1,36 @@
 from Bio import Entrez
 import pandas as pd
+from tqdm import tqdm
 
-Entrez.email = "soumyasinha.ai@gmail.com" 
+Entrez.email = "soumyasinha.ai@gmail.com"
 
 def fetch_pubmed_articles(pmids):
     records = []
 
-    for pmid in pmids:
-        handle = Entrez.efetch(db="pubmed", id=pmid, rettype="abstract", retmode="xml")
-        data = Entrez.read(handle)
+    for pmid in tqdm(pmids):
+        try:
+            handle = Entrez.efetch(
+                db="pubmed",
+                id=pmid,
+                rettype="abstract",
+                retmode="xml"
+            )
+            data = Entrez.read(handle)
 
-        article = data["PubmedArticle"][0]["MedlineCitation"]["Article"]
-        title = article["ArticleTitle"]
-        abstract = article["Abstract"]["AbstractText"][0] if "Abstract" in article else ""
+            article = data["PubmedArticle"][0]["MedlineCitation"]["Article"]
+            title = article.get("ArticleTitle", "")
 
-        records.append({
-            "pmid": pmid,
-            "title": title,
-            "text": abstract
-        })
+            abstract = ""
+            if "Abstract" in article:
+                abstract = " ".join(article["Abstract"]["AbstractText"])
+
+            records.append({
+                "pmid": pmid,
+                "title": title,
+                "text": abstract
+            })
+
+        except Exception as e:
+            print(f"Skipping PMID {pmid}: {e}")
 
     return pd.DataFrame(records)
